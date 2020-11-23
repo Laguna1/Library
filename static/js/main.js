ecb2fb5a40d303ed300f62e7e07989cdb95881dc
Laguna1 <lagunazoomarket@gmail.com>
2020-11-23 20:23:37 +0200

    Fix linter errors

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,11 @@
-window.onload = function () {
-  displayBooks();
-};
 
-let myLibrary = [];
-let booksDiv = document.getElementById("books");
-let newBookForm = document.getElementById("form");
-let newBookTitle = document.getElementById("title");
-let newBookAuthor = document.getElementById("author");
-let newBookPages = document.getElementById("pages");
-let newBookRead = document.getElementById("read");
+const myLibrary = [];
+const booksDiv = document.getElementById('books');
+const newBookForm = document.getElementById('form');
+const newBookTitle = document.getElementById('title');
+const newBookAuthor = document.getElementById('author');
+const newBookPages = document.getElementById('pages');
+const newBookRead = document.getElementById('read');
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -17,72 +14,76 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-Book.prototype.toggleReadStatus = function () {
-  this.read = !this.read;
-  displayBooks();
-};
-
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function deleteBookFromLibrary(e) {
-  myLibrary.splice(e.getAttribute("data-id"), 1);
-  displayBooks();
-}
-
-let book1 = new Book("Harry Potter", "J K Rowlings", 500, true);
-let book2 = new Book("Harry Potter2", "J K Rowlings", 344, false);
-let book3 = new Book("Harry Potter3", "J K Rowlings", 260, true);
+const book1 = new Book('Harry Potter', 'J K Rowlings', 500, true);
+const book2 = new Book('Harry Potter2', 'J K Rowlings', 344, false);
+const book3 = new Book('Harry Potter3', 'J K Rowlings', 260, true);
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
 
 function displayBooks() {
-  let books = ``;
+  let books = '';
   let index = 0;
   myLibrary.forEach((book) => {
     books += `
     <div class="book-card">
       <h4 class="title"><span class=" font-weight-bold">Title: </span><span class="value">${
-        book.title
-      }</span> </h4>
+  book.title
+}</span> </h4>
       <h4 class="title"><span class=" font-weight-bold">Author: </span><span class="value">${
-        book.author
-      }</span> </h4>
+  book.author
+}</span> </h4>
       <h4 class="title"><span class=" font-weight-bold">Pages:</span> <span class="value">${
-        book.pages
-      }</span> </h4>
+  book.pages
+}</span> </h4>
       <h4 class="title"><span class=" font-weight-bold">Read:</span> <span class="value">${
-        book.read ? "Yes" : "No"
-      }</span> </h4>
+  book.read ? 'Yes' : 'No'
+}</span> </h4>
       <button onclick="deleteBookFromLibrary(this)" data-id="${index}" class="btn btn-danger my-1">Delete Book</button>
       <button onclick="myLibrary[${index}].toggleReadStatus()" class="btn ${
-      book.read ? "btn-primary" : "btn-success"
-    } my-1">${book.read ? "Mark as unread" : "Mark as read"}</button>
+  book.read ? 'btn-primary' : 'btn-success'
+} my-1">${book.read ? 'Mark as unread' : 'Mark as read'}</button>
     </div>
     `;
-    index++;
+    index += 1;
   });
   booksDiv.innerHTML = books;
 }
 
-function showForm() {
-  newBookForm.style.display = "flex";
-}
+window.onload = function () {
+  displayBooks();
+};
 
-function createBook() {
-  let book = new Book(
-    newBookTitle.value,
-    newBookAuthor.value,
-    newBookPages.value,
-    newBookRead.checked
-  );
-  addBookToLibrary(book);
-  newBookForm.style.display = "none";
+function deleteBookFromLibrary(e) { // eslint-disable-line no-unused-vars
+  myLibrary.splice(e.getAttribute('data-id'), 1);
   displayBooks();
 }
 
-function cancelButton() {
-  newBookForm.style.display = "none";
+Book.prototype.toggleReadStatus = function () {
+  this.read = !this.read;
+  displayBooks();
+};
+
+function showForm() { // eslint-disable-line no-unused-vars
+  newBookForm.style.display = 'flex';
+}
+
+function createBook() { // eslint-disable-line no-unused-vars
+  const book = new Book(
+    newBookTitle.value,
+    newBookAuthor.value,
+    newBookPages.value,
+    newBookRead.checked,
+  );
+  addBookToLibrary(book);
+  newBookForm.style.display = 'none';
+  displayBooks();
+}
+
+function cancelButton() { // eslint-disable-line no-unused-vars
+  newBookForm.style.display = 'none';
 }
