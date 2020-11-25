c6a186c3977c2103de92cba380004512ea59e7c4
Laguna1 <lagunazoomarket@gmail.com>
2020-11-25 02:13:43 +0200

    Refactoring -v2

--- static/js/main2.js
+++ static/js/main2.js
@@ -1,30 +1,42 @@
-class Book {
-    constructor(title, author, pages, read) {
-      this.title = title;
-      this.author = author;
-      this.pages = pages;
-      this.read = read;
-    }
-  }
-  Book.prototype.toggleReadStatus = function () {
-    this.read = !this.read;
-  };
-  
-  const myLibrary = [];
-  
-  book1 = new Book("Harry Potter", "J K Rowlings", 500, true);
-  book2 = new Book("Harry Potter2", "J K Rowlings", 344, false);
-  book3 = new Book("Harry Potter3", "J K Rowlings", 260, true);
-  
-  function addBookToLibrary(book) {
-    myLibrary.push(book);
-  }
-  
-  function displayBooks() {
-    let books = "";
-    let index = 0;
-    myLibrary.forEach((book) => {
-      books += `
+// class Book {
+//     constructor(title, author, pages, read) {
+//       this.title = title;
+//       this.author = author;
+//       this.pages = pages;
+//       this.read = read;
+//     }
+//   }
+
+// Book.prototype.toggleReadStatus = function () {
+//   this.read = !this.read;
+// };
+
+//   const Nerd = (name) => {
+//     const prototype = Person(name)
+//     const doSomethingNerdy = () => console.log('nerd stuff')
+//     return Object.assign({}, prototype, {doSomethingNerdy})
+//   }
+const Book = (title, author, pages, read) => {
+  const Bok = { title, author, pages, read };
+  const toggleReadStatus = { read: !read };
+  return Object.assign({}, Bok, toggleReadStatus)
+}
+
+const myLibrary = [];
+
+book1 = Book("Harry Potter", "J K Rowlings", 500, true);
+book2 = Book("Harry Potter2", "J K Rowlings", 344, false);
+book3 = Book("Harry Potter3", "J K Rowlings", 260, true);
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function displayBooks() {
+  let books = "";
+  let index = 0;
+  myLibrary.forEach((book) => {
+    books += `
         <div class="book-card">
           <h4 class="title"><span class=" font-weight-bold">Title: </span><span class="value">${
             book.title
@@ -40,56 +52,55 @@ class Book {
           }</span> </h4>
           <button onclick="deleteBookFromLibrary(this)" data-id="${index}" class="btn btn-danger my-1">Delete Book</button>
           <button onclick="changeReadStatus(this)" data-id="${index}" class="btn ${
-        book.read ? "btn-primary" : "btn-success"
-      } my-1">${book.read ? "Mark as unread" : "Mark as read"}</button>
+      book.read ? "btn-primary" : "btn-success"
+    } my-1">${book.read ? "Mark as unread" : "Mark as read"}</button>
         </div>
         `;
-      index += 1;
-    });
-    booksDiv.innerHTML = books;
-  }
-  
-  window.onload = function () {
-    displayBooks();
-  };
-  
-  function deleteBookFromLibrary(e) {
-    myLibrary.splice(e.getAttribute("data-id"), 1);
-    displayBooks();
-  }
-  
-  function changeReadStatus(e) {
-    myLibrary[e.getAttribute("data-id")].toggleReadStatus();
-    displayBooks();
-  }
-  
-  function showForm() {
-    newBookForm.classList.toggle("display-none");
-  }
-  
-  function createBook() {
-    const book = Book(
-      newBookTitle.value,
-      newBookAuthor.value,
-      newBookPages.value,
-      newBookRead.checked
-    );
-    addBookToLibrary(book);
-    newBookForm.classList.toggle("display-none");
-    displayBooks();
-  }
-  
-  function cancelButton() {
-    newBookForm.classList.toggle("display-none");
-  }
-  
-  const booksDiv = document.getElementById("books");
-  const newBookForm = document.getElementById("form");
-  const newBookTitle = document.getElementById("title");
-  const newBookAuthor = document.getElementById("author");
-  const newBookPages = document.getElementById("pages");
-  const newBookRead = document.getElementById("read");
-  addBookToLibrary(book1);
-  addBookToLibrary(book2);
-  addBookToLibrary(book3);
-  
+    index += 1;
+  });
+  booksDiv.innerHTML = books;
+}
+
+window.onload = function () {
+  displayBooks();
+};
+
+function deleteBookFromLibrary(e) {
+  myLibrary.splice(e.getAttribute("data-id"), 1);
+  displayBooks();
+}
+
+function changeReadStatus(e) {
+  myLibrary[e.getAttribute("data-id")].toggleReadStatus();
+  displayBooks();
+}
+
+function showForm() {
+  newBookForm.classList.toggle("display-none");
+}
+
+function createBook() {
+  const book = Book(
+    newBookTitle.value,
+    newBookAuthor.value,
+    newBookPages.value,
+    newBookRead.checked
+  );
+  addBookToLibrary(book);
+  newBookForm.classList.toggle("display-none");
+  displayBooks();
+}
+
+function cancelButton() {
+  newBookForm.classList.toggle("display-none");
+}
+
+const booksDiv = document.getElementById("books");
+const newBookForm = document.getElementById("form");
+const newBookTitle = document.getElementById("title");
+const newBookAuthor = document.getElementById("author");
+const newBookPages = document.getElementById("pages");
+const newBookRead = document.getElementById("read");
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
